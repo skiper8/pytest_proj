7075a2ad9e8216285c5028fbdd03ccc7190006a5
askip <a.skiper2013@yandex.ru>
2023-01-31 11:42:30 +0300

    Исправил тесты

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -3,7 +3,7 @@ from utils import arrs
 
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
-    assert arrs.get([], -1, "test") == "test"
+    assert arrs.get([], -2, "test") == "test"
 
 
 def test_slice():
